ce4c5feabc5844449ea3a0d1a8b85596038250fb
CarlosBone <carlos.bone@novateva.com>
2024-03-10 23:34:40 -0400

    extraer estilos a otro archivo

---
+++ client/src/components/UserMonitor/styles.js
@@ -0,0 +1,16 @@
+export const styleUsuarioMonitorModal = {
+  position: 'absolute',
+  top: '20%',
+  left: '20%',
+  transform: 'translate(-10%, -10%)',
+  width: '80%',
+  minHeight: '400px',
+  height: '80%',
+  maxHeight: '80%',
+  bgcolor: 'background.paper',
+  border: '1px solid #666',
+  borderRadius: '16px',
+  boxShadow: 24,
+  // p: 4,
+  overflowY: 'hidden'
+};
