3347c723f68de9ac8e672531e204364ad23037ac
CarlosBone <carlos.bone@novateva.com>
2024-03-19 16:23:44 -0400

    configuracion adicional de jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   roots: ['<rootDir>/tests'],
+  globalSetup: '<rootDir>/tests/setup.js',
+  globalTeardown: '<rootDir>/tests/teardown.js',
 };
+
+module.exports = config;
---
+++ tests/setup.js
@@ -0,0 +1,12 @@
+/**
+ * Generate a function comment for the given function body.
+ *
+ * @param {import('@jest/types').Config.GlobalConfig} globalConfig - the global configuration object
+ * @param {import('@jest/types').Config.ProjectConfig} projectConfig - the project configuration object
+ * @return {Promise<void>} a Promise that resolves when the function completes
+ */
+module.exports = async function (globalConfig, projectConfig) {
+  // console.log('MARCO');
+
+
+};
