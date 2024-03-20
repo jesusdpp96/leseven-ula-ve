195f8b0e885aa9e99189bf41891cbbc19e3f1352
CarlosBone <carlos.bone@novateva.com>
2024-03-19 20:43:38 -0400

    renderizar pruebas en la vista correspondiente

--- client/src/components/UserMonitor/PruebasMonitor/PruebasMonitor.js
+++ client/src/components/UserMonitor/PruebasMonitor/PruebasMonitor.js
@@ -9,63 +9,60 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
+import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import customFetch from "../../../utils/request";
+import { getDateDDMMYYYY } from "../../../utils/dates";
 
 export default function PruebasMonitor() {
 
-  // const {userId} = useParams();
-  const {isLoading: loading} = useQuery('usersData', () => customFetch('/aprendices-monitor/users'));
+  const { userId } = useParams();
+  const { isLoading: loading, data: practicas } = useQuery('practicasData', () => customFetch(`/practica/${userId}`));
 
+  if (loading) 
+    return <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <CircularProgress color="primary" size={50} style={{ margin: 'auto' }} />
+    </Box> 
+  
   return (
-    <Box sx={{display: 'flex', flexDirection: 'column'}}>
-
-      <Box sx={{margin: "32px 0"}}>
-        <Typography component="h1"  variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-          Pruebas realizadas por el alumno {loading && (<CircularProgress color="primary" size={20} />)}
+    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <Box sx={{ margin: "32px 0" }}>
+        <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+          Pruebas realizadas por el alumno
         </Typography>
       </Box>
-      <Box>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="right">Fecha</TableCell>
-                <TableCell align="right">Grado</TableCell>
-                <TableCell align="right">Total de preguntas</TableCell>
-                <TableCell align="right">Respuestas correctas</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {/* {usersData.map((row) => (
-                <TableRow
-                  key={row.correo}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    <Button variant="text" onClick={() => goToDetails(row.id)}>
-                      {`${row.nombre} ${row.apellido}`}
-                    </Button>
-                  </TableCell>
-                  <TableCell align="right">{row.correo}</TableCell>
-                  <TableCell align="right">{row.practicas_realizadas}</TableCell>
-                  <TableCell align="right">{row.consultas_correctas}</TableCell>
-                  <TableCell align="right">{row.consultas_incorrectas}</TableCell>
-                  <TableCell align="right">
-                    {removeLoading !== row.correo ?
-                     (<IconButton color="error"
-                      aria-label="remove element"
-                      component="span"
-                      onClick={async () => { await removeUser({correo: row.correo})}}>
-                        <DeleteForeverIcon />
-                      </IconButton>) : (<CircularProgress color="error" size={20} />)}
-                  </TableCell>
+      {practicas?.length ?
+        (<Box>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="left">Fecha</TableCell>
+                  <TableCell align="left">Grado</TableCell>
+                  <TableCell align="left">Total de preguntas</TableCell>
+                  <TableCell align="left">Respuestas correctas</TableCell>
                 </TableRow>
-              ))} */}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Box>
+              </TableHead>
+              <TableBody>
+                {practicas.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell align="left">{getDateDDMMYYYY(new Date(row.fecha))}</TableCell>
+                    <TableCell align="left">{row.grado_id}</TableCell>
+                    <TableCell align="left">{row.total_consultas}</TableCell>
+                    <TableCell align="left">{row.total_correctas}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+        ) : (<Typography component="h3" variant="h7" color="inherit" noWrap sx={{ flexGrow: 1, textAlign: 'center' }}>
+          El alumno no ha realizado ninguna prueba
+        </Typography>)
+      }
     </Box>
   );
 }
